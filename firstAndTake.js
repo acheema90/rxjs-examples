b638eb8973d61cd0ed9dd0b5a25f47a1ee76072c
Ajaypal Cheema <ajaypalster@gmail.com>
2020-11-16 09:58:42 -0500

    first and take operators

--- firstAndTake.js
+++ firstAndTake.js
@@ -1,21 +1,6 @@
 import { fromEvent, of } from 'rxjs';
 import { map, take, first } from 'rxjs/operators';
 
-function calculateScrollPercent(element) {
-	const { scrollTop, scrollHeight, clientHeight } = element;
-	return (scrollTop / (scrollHeight - clientHeight)) * 100;
-}
-
-const progressBar = document.querySelector('.progress-bar');
-
-const scroll$ = fromEvent(document, 'scroll').pipe(
-	map((event) => calculateScrollPercent(event.target.scrollingElement))
-);
-
-scroll$.subscribe((val) => {
-	progressBar.style.width = `${val}%`;
-});
-
 const observer = {
 	next: (val) => console.info('next', val),
 	error: (error) => console.info('error', error),
